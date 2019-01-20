8deb599806f33b551717986bb13ea4e0983fa0f9
Preslav Rachev <rachev.preslav@gmail.com>
2019-01-20 18:07:50 +0100

    Replaces Disqus with GitHub comments

--- src/js/script.js
+++ src/js/script.js
@@ -136,24 +136,4 @@ jQuery(function($) {
 		$('#wrapper').fitVids();
 	}
 	video();
-
-	/* ==========================================================================
-	   Initialize and load Disqus
-	   ========================================================================== */
-
-	if (typeof disqus === 'undefined') {
-		$('.post-comments').css({
-			'display' : 'none'
-		});
-	} else {
-		$('#show-disqus').on('click', function() {
-			$.ajax({
-				type: "GET",
-				url: "//" + disqus + ".disqus.com/embed.js",
-				dataType: "script",
-				cache: true
-			});
-			$(this).parent().addClass('activated');
-		});
-	}
 });
